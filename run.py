ad8f470c64ce3ea138c5a8030698b5da143ccf55
Chris <christian.borza@students.codeinstitute.net>
2024-10-14 15:45:23 +0200

    Create main menu function

--- run.py
+++ run.py
@@ -12,23 +12,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('workouts')
 
-# connect with the google sheet
+# Pull exercises from google spreadsheet
 sheet_exercises = SHEET.worksheet("exercises")
 exercises_data = sheet_exercises.get_all_records()
 
-for exercise in exercises_data:
-    print(exercise)
-
+# Save new workouts to spreadsheet
 sheet_saved_workouts = SHEET.worksheet("saved_workouts")
-workout_to_save = [
-    "2024-10-15",
-    "Workout",
-    "Chest",
-    "Push-Ups",
-    15,
-    60,
-    "medium"
-]
-
+workout_to_save = []
 sheet_saved_workouts.append_row(workout_to_save)
-print("Workout successfully saved")
+# print("Workout successfully saved")
+
+def main_menu():
+    print("Welcome to the Workout Generator")
+    print("1. Create a new workout")
+    print("2. Show saved workouts")
+
+    choice = input("Please choose an option (1 or 2): ")
+
+    if choice == "1":
+        create_workout()
+    elif choice == "2":
+        show_saved_workouts()
+    else:
+        print("Invalid input, please choose 1 or 2.")
+        main_menu()
+
+main_menu()
